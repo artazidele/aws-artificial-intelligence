03c324ff764b5823a5e6702e608e8410b244bcb8
artazidele <77989249+artazidele@users.noreply.github.com>
2023-05-25 10:36:56 +0300

    Created repository

---
+++ app/src/main/java/com/example/ml_aws/AwsMlApplication.kt
@@ -0,0 +1,26 @@
+package com.example.ml_aws
+
+import android.app.Application
+import android.util.Log
+import com.amplifyframework.AmplifyException
+import com.amplifyframework.auth.cognito.AWSCognitoAuthPlugin
+import com.amplifyframework.core.Amplify
+import com.amplifyframework.predictions.aws.AWSPredictionsPlugin
+
+class AwsMlApplication: Application() {
+    override fun onCreate() {
+        super.onCreate()
+
+        try {
+            // Spraudnis autentifikācijas kategorijai
+            Amplify.addPlugin(AWSCognitoAuthPlugin())
+            // Spraudnis mākslīgā intelekta kategorijai
+            Amplify.addPlugin(AWSPredictionsPlugin())
+
+            // Amplify inicializēšana
+            Log.i("MyAmplifyApp", "Initialized Amplify")
+        } catch (error: AmplifyException) {
+            Log.e("MyAmplifyApp", "Could not initialize Amplify", error)
+        }
+    }
+}
---
+++ app/src/main/java/com/example/ml_aws/MainActivity.kt
@@ -0,0 +1,83 @@
+package com.example.ml_aws
+
+import android.media.MediaPlayer
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import com.amplifyframework.core.Amplify
+import com.amplifyframework.predictions.PredictionsException
+import java.io.*
+
+class MainActivity : AppCompatActivity() {
+
+    private val mp = MediaPlayer()
+
+    private lateinit var textBtn: Button
+    private lateinit var translateBtn: Button
+    private lateinit var textV: TextView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        textBtn = findViewById(R.id.text_to_speech_btn)
+        translateBtn = findViewById(R.id.translate_text_btn)
+        textV = findViewById(R.id.text_view)
+
+        textBtn.setOnClickListener {
+            convertTextToSpeech()
+        }
+
+        translateBtn.setOnClickListener {
+            val text = "I like to eat spaghetti"
+            translateText(text)
+        }
+    }
+
+    // Funkcija teksta pārvēršanai uz runu
+    private fun convertTextToSpeech() {
+        Amplify.Predictions.convertTextToSpeech("I like to eat spaghetti!",
+            {
+                textV.text = "Text is converted to speech."
+                playAudio(it.audioData)
+            }, {
+                textV.text = "Text is not converted to speech. There was an error."
+            }
+        )
+    }
+
+    // Funkcija audio faila izveidei
+    private fun playAudio(data: InputStream) {
+        val mp3File = File(cacheDir, "audio.mp3")
+        try {
+            FileOutputStream(mp3File).use { out ->
+                val buffer = ByteArray(8 * 1024)
+                var bytesRead: Int
+                while (data.read(buffer).also { bytesRead = it } != -1) {
+                    out.write(buffer, 0, bytesRead)
+                }
+                mp.reset()
+                mp.setOnPreparedListener { obj: MediaPlayer -> obj.start() }
+                mp.setDataSource(FileInputStream(mp3File).fd)
+                mp.prepareAsync()
+            }
+        } catch (error: IOException) {
+            textV.text = "Audio file is not created. There was an error."
+        }
+    }
+
+    // Funkcija teksta tulkošanai
+    private fun translateText(text: String) {
+        try {
+            Amplify.Predictions.translateText(text,
+                { it ->
+                    textV.text = "Text is translated: " + it.translatedText
+                }, {
+                    textV.text = "Text is not translated. There was an error."
+                })
+        } catch (error: PredictionsException) {
+            textV.text = "Text is not translated. There was an error."
+        }
+    }
+}
